f0bf6c9df5e190b721b96b0159f452fe3f4a6c88
Bibin Babu <bibin6724@gmail.com>
2022-05-07 13:03:27 +0530

    movement of left and right paddle defined using functions

--- main.py
+++ main.py
@@ -1,37 +1,65 @@
-#simple pong_Game
+# simple pong_Game
 
 import turtle as t
-wn=t.Screen()
-wn.setup(width=800,height=600)
+
+wn = t.Screen()
+wn.setup(width=800, height=600)
 wn.bgcolor("black")
 wn.tracer(0)
 wn.title("PONG by Bibin")
 
-#paddle A
-paddle_a=t.Turtle()
+# paddle A
+paddle_a = t.Turtle()
 paddle_a.speed(0)
 paddle_a.shape("square")
 paddle_a.color("white")
-paddle_a.shapesize(stretch_wid=5,stretch_len=1)
+paddle_a.shapesize(stretch_wid=5, stretch_len=1)
 paddle_a.penup()
-paddle_a.goto(-350,0)
+paddle_a.goto(-350, 0)
 
-#Paddle B
-paddle_b=t.Turtle()
+# Paddle B
+paddle_b = t.Turtle()
 paddle_b.speed(0)
 paddle_b.shape("square")
 paddle_b.color("white")
-paddle_b.shapesize(stretch_wid=5,stretch_len=1)
+paddle_b.shapesize(stretch_wid=5, stretch_len=1)
 paddle_b.penup()
-paddle_b.goto(350,0)
-#Ball
-ball=t.Turtle()
+paddle_b.goto(350, 0)
+# Ball
+ball = t.Turtle()
 ball.speed(0)
 ball.shape("square")
 ball.color("white")
 ball.penup()
-ball.goto(0,0)
+ball.goto(0, 0)
 
 
+# function
+def paddle_a_up():
+    y = paddle_a.ycor()
+    y += 20
+    paddle_a.sety(y)
+def paddle_a_down():
+    y = paddle_a.ycor()
+    y -= 20
+    paddle_a.sety(y)
+
+def paddle_b_up():
+    y = paddle_b.ycor()
+    y += 20
+    paddle_b.sety(y)
+def paddle_b_down():
+    y = paddle_b.ycor()
+    y -= 20
+    paddle_b.sety(y)
+
+
+# keyboard binding
+wn.listen()
+wn.onkeypress(paddle_a_up, "w")
+wn.onkeypress(paddle_a_down,"s")
+wn.onkeypress(paddle_b_up, "Up")
+wn.onkeypress(paddle_b_down,"Down")
+# main Game_Loop
 while True:
     wn.update()
